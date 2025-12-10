b97bfe2e7afaa733cc7facba33e2fbadfea55ae8
Md Shakir Ansari <118336491+Sakku9631@users.noreply.github.com>
2025-12-10 17:13:17 +0530

    Add ERC20Mock contract for testing ERC20 tokens
    This contract allows minting of ERC20 tokens for testing purposes.

---
+++ ERC20Mock.sol
@@ -0,0 +1,23 @@
+
+``solidity name=ERC20Mock.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/// Simple ERC20 mintable token for testing in Remix.
+/// Not intended for production.
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v4.9.3/contracts/token/ERC20/ERC20.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v4.9.3/contracts/access/Ownable.sol";
+
+contract ERC20Mock is ERC20, Ownable {
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
+
+    /// @notice Mint tokens to `to`. Only owner (for testing).
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    /// @notice Convenience to mint tokens to the caller in Remix.
+    function mintToSelf(uint256 amount) external {
+        _mint(msg.sender, amount);
+    }
+}
