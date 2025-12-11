ab1c45b6d9b1b8c9b931a4fac5d5afe967857a8c
Md Shakir Ansari <118336491+Sakku9631@users.noreply.github.com>
2025-12-11 21:52:28 +0530

    Update IStrategy.sol

--- IStrategy.sol
+++ IStrategy.sol
@@ -16,3 +16,4 @@ Multi-Chain-Asset-Aggregator
 Solidity Yield Farming Example
 DAO-Controlled Strategy Governance System
 AI-Powered DeFi Yield Optimizer
+qn-guide-examples/sample-dapps/ai-powered-defi-yield-optimizer
