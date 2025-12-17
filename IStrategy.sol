de73d944804011a0c8639610122025b3003672c1
Md Shakir Ansari <118336491+Sakku9631@users.noreply.github.com>
2025-12-17 13:42:04 +0530

    Update IStrategy.sol

--- IStrategy.sol
+++ IStrategy.sol
@@ -17,5 +17,4 @@ Solidity Yield Farming Example
 obinnafranklinduru / defi-yield-aggregator
 machenxi/solana-yield-farmer
 Concentrated Liquidity Management
-Beefy Finance (BIFI) Strategy Contracts
 Auto-Compounding
