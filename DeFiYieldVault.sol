390beb1d1ff975c28e7f38b14a40ea7d8409565f
Md Shakir Ansari <118336491+Sakku9631@users.noreply.github.com>
2026-01-04 14:17:01 +0530

    Update DeFiYieldVault.sol

--- DeFiYieldVault.sol
+++ DeFiYieldVault.sol
@@ -213,5 +213,4 @@ contract DeFiYieldVault is ReentrancyGuard, Ownable, Pausable {
 }
 Dyslex7c / DefiYieldOptimizer
 Yield Trading and Tokenization
-Yearn Finance (The Pioneer)
 Asset-Specific Strategies
