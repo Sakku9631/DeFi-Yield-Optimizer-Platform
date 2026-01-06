4801b95c744d4b8d58d225fbb992baca2db2ea02
Md Shakir Ansari <118336491+Sakku9631@users.noreply.github.com>
2026-01-06 18:30:09 +0530

    Remove comments about MakerDAO and Enricrypto

--- ERC20Mock.sol
+++ ERC20Mock.sol
@@ -21,7 +21,6 @@ contract ERC20Mock is ERC20, Ownable {
         _mint(msg.sender, amount);
     }
 }
-MakerDAO - DSR Strategy
 Impermanent Loss (IL) Mitigation Optimizer
 
 Enricrypto / Isolated-Lending-Market
